77720c4251766a7ab2cadd7f3cbdf53990db855a
Antonio Malara <antonio.malara@booking.com>
2014-08-16 04:19:00 +0200

    Initial Commit

---
+++ adiumcat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  adiumcat
+//
+//  Created by Antonio Malara on 16/08/14.
+//  Copyright (c) 2014 Antonio Malara. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
